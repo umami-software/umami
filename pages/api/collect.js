132bbcbe0db1ee52227b6d49c195366dce83ac47
Mike Cao <mike@mikecao.com>
2020-07-20 01:54:21 -0700

    Refactored session and collect process.

--- pages/api/collect.js
+++ pages/api/collect.js
@@ -1,25 +1,30 @@
-import { parseCollectRequest } from 'lib/utils';
+import { parseSession } from 'lib/utils';
 import { savePageView, saveEvent } from 'lib/db';
 import { allowPost } from 'lib/middleware';
+import checkSession from 'lib/session';
 
 export default async (req, res) => {
   await allowPost(req, res);
 
-  const values = parseCollectRequest(req);
+  const session = await checkSession(req);
 
-  if (values.success) {
-    const { type, website_id, session_id, url, referrer, event_type, event_value } = values;
+  const { website_id, session_id } = parseSession(session);
+  const { type, payload } = req.body;
+  let ok = 1;
 
-    if (type === 'pageview') {
-      await savePageView(website_id, session_id, url, referrer).catch(() => {
-        values.success = 0;
-      });
-    } else if (type === 'event') {
-      await saveEvent(website_id, session_id, url, event_type, event_value).catch(() => {
-        values.success = 0;
-      });
-    }
+  if (type === 'pageview') {
+    const { url, referrer } = payload;
+    await savePageView(website_id, session_id, url, referrer).catch(e => {
+      ok = 0;
+      throw e;
+    });
+  } else if (type === 'event') {
+    const { url, event_type, event_value } = payload;
+    await saveEvent(website_id, session_id, url, event_type, event_value).catch(() => {
+      ok = 0;
+      throw e;
+    });
   }
 
-  res.status(200).json({ success: values.success });
+  res.status(200).json({ ok, session });
 };
--- scripts/umami/index.js
+++ scripts/umami/index.js
@@ -1,134 +1,111 @@
 import 'promise-polyfill/src/polyfill';
 import 'unfetch/polyfill';
 
-const {
-  screen: { width, height },
-  navigator: { language },
-  location: { hostname, pathname, search },
-  localStorage: store,
-  document,
-  history,
-} = window;
+((window, sessionKey) => {
+  const {
+    screen: { width, height },
+    navigator: { language },
+    location: { hostname, pathname, search },
+    localStorage: store,
+    document,
+    history,
+  } = window;
 
-/* Load script */
+  const script = document.querySelector('script[data-website-id]');
+  const website = script && script.getAttribute('data-website-id');
+  const hostUrl = new URL(script.src).origin;
+  const screen = `${width}x${height}`;
+  const listeners = [];
 
-const script = document.querySelector('script[data-website-id]');
+  let currentUrl = `${pathname}${search}`;
+  let currentRef = document.referrer;
 
-if (script) {
-  const website_id = script.getAttribute('data-website-id');
+  /* Helper methods */
 
-  if (website_id) {
-    const sessionKey = 'umami.session';
-    const hostUrl = new URL(script.src).origin;
-    const screen = `${width}x${height}`;
-    let currentUrl = `${pathname}${search}`;
-    let currenrRef = document.referrer;
-    const listeners = [];
+  const post = (url, params) =>
+    fetch(url, {
+      method: 'post',
+      cache: 'no-cache',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    }).then(res => res.json());
 
-    /* Helper methods */
-
-    const post = (url, params) =>
-      fetch(url, {
-        method: 'post',
-        cache: 'no-cache',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(params),
-      }).then(res => res.json());
-
-    const createSession = data =>
-      post(`${hostUrl}/api/session`, data).then(({ success, ...session }) => {
-        if (success) {
-          store.setItem(sessionKey, JSON.stringify(session));
-          return success;
-        }
-      });
-
-    const getSession = () => JSON.parse(store.getItem(sessionKey));
-
-    const getSessionData = url => ({ website_id, hostname, url, screen, language });
-
-    const pageView = (url, referrer) =>
-      post(`${hostUrl}/api/collect`, {
-        type: 'pageview',
-        payload: { url, referrer, session: getSession() },
-      }).then(({ success }) => {
-        if (!success) {
-          store.removeItem(sessionKey);
-        }
-        return success;
-      });
-
-    const trackEvent = (url, event_type, event_value) =>
-      post(`${hostUrl}/api/collect`, {
-        type: 'event',
-        payload: { url, event_type, event_value, session: getSession() },
-      });
-
-    const execute = (url, referrer) => {
-      const data = getSessionData(url);
-
-      if (!store.getItem(sessionKey)) {
-        createSession(data).then(success => success && pageView(url, referrer));
-      } else {
-        pageView(url, referrer).then(
-          success =>
-            !success && createSession(data).then(success => success && pageView(url, referrer)),
-        );
-      }
+  const collect = (type, params) => {
+    const payload = {
+      session: store.getItem(sessionKey),
+      url: currentUrl,
+      referrer: currentRef,
+      website,
+      hostname,
+      screen,
+      language,
     };
 
-    /* Handle push state */
-
-    const handlePush = (state, title, url) => {
-      removeEvents();
-      currenrRef = currentUrl;
-      currentUrl = url;
-      execute(currentUrl, currenrRef);
-      setTimeout(loadEvents, 300);
-    };
-
-    const hook = (type, cb) => {
-      const orig = history[type];
-      return (state, title, url) => {
-        const args = [state, title, url];
-        cb.apply(null, args);
-        return orig.apply(history, args);
-      };
-    };
-
-    history.pushState = hook('pushState', handlePush);
-    history.replaceState = hook('replaceState', handlePush);
-
-    /* Handle events */
-
-    const removeEvents = () => {
-      listeners.forEach(([element, type, listener]) => {
-        element.removeEventListener(type, listener, true);
+    if (params) {
+      Object.keys(params).forEach(key => {
+        payload[key] = params[key];
       });
-      listeners.length = 0;
-    };
+    }
 
-    const loadEvents = () => {
-      document.querySelectorAll("[class*='umami--']").forEach(element => {
-        element.className.split(' ').forEach(className => {
-          if (/^umami--/.test(className)) {
-            const [, type, value] = className.split('--');
-            if (type && value) {
-              const listener = () => trackEvent(currentUrl, type, value);
-              listeners.push([element, type, listener]);
-              element.addEventListener(type, listener, true);
-            }
+    return post(`${hostUrl}/api/collect`, {
+      type,
+      payload,
+    }).then(({ ok, session }) => ok && session && store.setItem(sessionKey, session));
+  };
+
+  const pageView = () => collect('pageview').then(() => setTimeout(loadEvents, 300));
+
+  const pageEvent = (event_type, event_value) => collect('event', { event_type, event_value });
+
+  /* Handle history */
+
+  const handlePush = (state, title, url) => {
+    removeEvents();
+    currentRef = currentUrl;
+    currentUrl = url;
+    pageView();
+  };
+
+  const hook = (type, cb) => {
+    const orig = history[type];
+    return (state, title, url) => {
+      const args = [state, title, url];
+      cb.apply(null, args);
+      return orig.apply(history, args);
+    };
+  };
+
+  history.pushState = hook('pushState', handlePush);
+  history.replaceState = hook('replaceState', handlePush);
+
+  /* Handle events */
+
+  const removeEvents = () => {
+    listeners.forEach(([element, type, listener]) => {
+      element.removeEventListener(type, listener, true);
+    });
+    listeners.length = 0;
+  };
+
+  const loadEvents = () => {
+    document.querySelectorAll("[class*='umami--']").forEach(element => {
+      element.className.split(' ').forEach(className => {
+        if (/^umami--/.test(className)) {
+          const [, type, value] = className.split('--');
+          if (type && value) {
+            const listener = () => pageEvent(type, value);
+            listeners.push([element, type, listener]);
+            element.addEventListener(type, listener, true);
           }
-        });
+        }
       });
-    };
+    });
+  };
 
-    /* Start */
+  /* Start */
 
-    execute(currentUrl, currenrRef);
-    loadEvents();
-  }
-}
+  pageView();
+})(window, 'umami.session');
