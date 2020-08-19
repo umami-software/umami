d4ca02f1108e20a412b09cc47ef2e334d3310f7e
Mike Cao <mike@mikecao.com>
2020-08-18 23:35:26 -0700

    Use portal for modals. Prisma disconnect after query.

--- components/common/Modal.js
+++ components/common/Modal.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { useSpring, animated } from 'react-spring';
 import styles from './Modal.module.css';
 
 export default function Modal({ title, children }) {
   const props = useSpring({ opacity: 1, from: { opacity: 0 } });
 
-  return (
+  return ReactDOM.createPortal(
     <animated.div className={styles.modal} style={props}>
       <div className={styles.content}>
         {title && <div className={styles.header}>{title}</div>}
         <div className={styles.body}>{children}</div>
       </div>
-    </animated.div>
+    </animated.div>,
+    document.getElementById('__modals'),
   );
 }
