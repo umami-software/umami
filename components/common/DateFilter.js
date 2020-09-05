4cc75e75d4c056ce4f2ccd55feb2920d3cdb94f8
Daksh Shah <dakshshah96@gmail.com>
2020-09-05 22:10:53 +0530

    Rearrange date range dropdown logically

--- components/common/DateFilter.js
+++ components/common/DateFilter.js
@@ -3,13 +3,12 @@ import { getDateRange } from 'lib/date';
 import DropDown from './DropDown';
 
 const filterOptions = [
-  { label: 'Last 24 hours', value: '24hour' },
-  { label: 'Last 7 days', value: '7day' },
-  { label: 'Last 30 days', value: '30day' },
-  { label: 'Last 90 days', value: '90day' },
   { label: 'Today', value: '1day' },
+  { label: 'Last 7 days', value: '7day' },
   { label: 'This week', value: '1week' },
+  { label: 'Last 30 days', value: '30day' },
   { label: 'This month', value: '1month' },
+  { label: 'Last 90 days', value: '90day' },
   { label: 'This year', value: '1year' },
 ];
 
