943c2c73d476eeac475135e931540b79d2ee3ee4
Mike Cao <mike@mikecao.com>
2020-09-10 20:37:07 -0700

    Added check for bots.

--- pages/api/collect.js
+++ pages/api/collect.js
@@ -1,8 +1,13 @@
 import { savePageView, saveEvent } from 'lib/queries';
 import { useCors, useSession } from 'lib/middleware';
 import { ok, badRequest } from 'lib/response';
+import isBot from 'isbot-fast';
 
 export default async (req, res) => {
+  if (isBot(req.headers['user-agent'])) {
+    return ok(res);
+  }
+
   await useCors(req, res);
   await useSession(req, res);
 
