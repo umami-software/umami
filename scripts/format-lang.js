6ea1bd13539d02c61fe0e634acb9f563616e49ac
Mike Cao <mike@mikecao.com>
2020-09-12 04:19:33 -0700

    New language build process.

---
+++ scripts/format-lang.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const prettier = require('prettier');
+
+const src = path.resolve(__dirname, '../lang');
+const dest = path.resolve(__dirname, '../lang-formatted');
+const files = fs.readdirSync(src);
+
+if (!fs.existsSync(dest)) {
+  fs.mkdirSync(dest);
+}
+
+files.forEach(file => {
+  const lang = require(`../lang/${file}`);
+  const keys = Object.keys(lang).sort();
+
+  const formatted = keys.reduce((obj, key) => {
+    obj[key] = { defaultMessage: lang[key] };
+    return obj;
+  }, {});
+
+  const json = prettier.format(JSON.stringify(formatted), { parser: 'json' });
+
+  fs.writeFileSync(path.resolve(dest, file), json);
+
+  console.log(path.resolve(src, file), '->', path.resolve(dest, file));
+});
