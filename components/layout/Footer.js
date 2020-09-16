5e57b26e46e7ab2b0eef9179913c2de5971dbee2
Mike Cao <mike@mikecao.com>
2020-09-15 19:16:05 -0700

    Convert buttons to links.

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import classNames from 'classnames';
-import Button from 'components/common/Button';
-import Logo from 'assets/logo.svg';
 import styles from './Footer.module.css';
 
 export default function Footer() {
   const version = process.env.VERSION;
   return (
     <footer className="container">
-      <div className={classNames(styles.footer, 'row justify-content-center')}>
-        <FormattedMessage id="footer.powered-by" defaultMessage="Powered by" />
-        <a href="https://umami.is">
-          <Button className={styles.button} icon={<Logo />} size="small">
-            <b>umami</b>
-          </Button>
-        </a>
+      <div className={styles.footer}>
+        <div />
+        <div>
+          <FormattedMessage
+            id="footer.powered-by"
+            defaultMessage="Powered by {name} {version}"
+            values={{
+              name: (
+                <a href="https://umami.is">
+                  <b>umami</b>
+                </a>
+              ),
+            }}
+          />
+        </div>
         <div>{`v${version}`}</div>
       </div>
     </footer>
