90999fa5e847cf8f4bafb5de60eece6626fe9e7f
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-09-23 18:35:37 +0530

    Added vercel cors

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 require('dotenv').config();
-const pkg = require('./package.json');
 
 module.exports = {
-  env: {
-    VERSION: pkg.version,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -15,5 +11,23 @@ module.exports = {
     });
 
     return config;
+  },
+  async headers() {
+    return [
+      {
+        // mathching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
   },
 };
