d1765f746b9667fd7aea7f3c29436af22ea2efe5
Mike Cao <mike@mikecao.com>
2020-09-26 20:46:20 -0700

    Updated next config.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ module.exports = {
   env: {
     VERSION: pkg.version,
   },
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
