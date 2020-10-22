5b84378731d0fb3a170df913a27a3f2542b5aa23
Mike Cao <mike@mikecao.com>
2020-10-22 10:36:08 -0700

    Added Czech language.

--- scripts/check-lang.js
+++ scripts/check-lang.js
@@ -7,11 +7,16 @@ const ignore = require('../lang-ignore.json');
 const dir = path.resolve(__dirname, '../lang');
 const files = fs.readdirSync(dir);
 const keys = Object.keys(messages).sort();
+const filter = process.argv?.[2];
 
 files.forEach(file => {
   if (file !== 'en-US.json') {
     const lang = require(`../lang/${file}`);
     const id = file.replace('.json', '');
+
+    if (filter && filter !== id) {
+      return;
+    }
 
     console.log(chalk.yellowBright(`\n## ${file.replace('.json', '')}`));
     let count = 0;
