84dda6819b0922190e0b609678e91e7c74e1396e
Adrian Setyadi <a.styd@yahoo.com>
2020-11-17 20:49:49 +0700

    Fix bounce rate miscalculation

--- components/metrics/MetricsBar.js
+++ components/metrics/MetricsBar.js
@@ -60,7 +60,7 @@ export default function MetricsBar({ websiteId, className }) {
           />
           <MetricCard
             label={<FormattedMessage id="metrics.bounce-rate" defaultMessage="Bounce rate" />}
-            value={pageviews ? (bounces / pageviews) * 100 : 0}
+            value={uniques ? (bounces / uniques) * 100 : 0}
             format={n => Number(n).toFixed(0) + '%'}
           />
           <MetricCard
