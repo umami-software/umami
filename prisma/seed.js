3925258bc7defefec327ca32ebc54a98ec85dd74
Beorn Facchini <beornf@gmail.com>
2021-03-20 23:01:10 +1100

    Prisma Migrate with database seeding fixes #559

---
+++ prisma/seed.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const SALT_ROUNDS = 10;
+
+const hashPassword = password => {
+  return bcrypt.hash(password, SALT_ROUNDS);
+};
+
+async function main() {
+  const password = await hashPassword(process.env.ADMIN_PASSWORD || 'umami');
+  await prisma.account.upsert({
+    where: { username: 'admin' },
+    update: {},
+    create: {
+      username: 'admin',
+      password: password,
+      is_admin: true,
+    },
+  });
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
