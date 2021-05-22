00df8184cd613b6d4170afa65f302c24118da28b
Philipp Dormann <philipp@philippdormann.de>
2021-05-22 17:36:25 +0200

    Footer cleanup

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -6,28 +6,24 @@ import styles from './Footer.module.css';
 import useVersion from 'hooks/useVersion';
 
 export default function Footer() {
-  const { current } = useVersion();
-  return (
-    <footer className="container">
-      <div className={classNames(styles.footer, 'row')}>
-        <div className="col-12 col-md-4" />
-        <div className="col-12 col-md-4">
-          <FormattedMessage
-            id="message.powered-by"
-            defaultMessage="Powered by {name}"
-            values={{
-              name: (
-                <Link href="https://umami.is">
-                  <b>umami</b>
-                </Link>
-              ),
-            }}
-          />
-        </div>
-        <div className={classNames(styles.version, 'col-12 col-md-4')}>
-          <Link href={`https://github.com/mikecao/umami/releases`}>{`v${current}`}</Link>
-        </div>
-      </div>
-    </footer>
-  );
+	const { current } = useVersion();
+	return (
+		<footer className="container">
+			<div className={classNames(styles.footer, 'row')}>
+				<div className="col-12 col-md-4" />
+				<div className="col-12 col-md-4">
+					<a href="https://umami.is">
+						<b>umami-sqlite</b>
+					</a>{' '}
+					powered by{' '}
+					<a href="https://umami.is">
+						<b>umami</b>
+					</a>
+				</div>
+				<div className={classNames(styles.version, 'col-12 col-md-4')}>
+					<Link href={`https://github.com/mikecao/umami/`}>GitHub</Link>
+				</div>
+			</div>
+		</footer>
+	);
 }
