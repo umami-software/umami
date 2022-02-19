460762bf3d11b6e3938fa1270e807bc43a8f75a4
Mike Cao <mike@mikecao.com>
2022-02-18 20:00:21 -0800

    Use rewrites for tracker script name, closes #972

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const pkg = require('./package.json');
 
+const { BASE_PATH, FORCE_SSL, DISABLE_LOGIN, TRACKER_SCRIPT_NAME } = process.env;
+
 module.exports = {
   env: {
     VERSION: pkg.version,
-    FORCE_SSL: !!process.env.FORCE_SSL,
-    DISABLE_LOGIN: !!process.env.DISABLE_LOGIN,
-    TRACKER_SCRIPT_NAME: process.env.TRACKER_SCRIPT_NAME,
+    FORCE_SSL: Boolean(FORCE_SSL),
+    DISABLE_LOGIN: Boolean(DISABLE_LOGIN),
+    TRACKER_SCRIPT_NAME,
   },
-  basePath: process.env.BASE_PATH,
+  basePath: BASE_PATH,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -21,10 +23,15 @@ module.exports = {
 
     return config;
   },
+  async rewrites() {
+    return TRACKER_SCRIPT_NAME
+      ? [{ source: `/${TRACKER_SCRIPT_NAME}.js`, destination: '/umami.js' }]
+      : [];
+  },
   async headers() {
     return [
       {
-        source: '/umami.js',
+        source: `/${TRACKER_SCRIPT_NAME || 'umami'}.js`,
         headers: [
           {
             key: 'Cache-Control',
--- rollup.tracker.config.js
+++ rollup.tracker.config.js
@@ -3,12 +3,10 @@ import buble from '@rollup/plugin-buble';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
-const scriptName = process.env.TRACKER_SCRIPT_NAME || 'umami';
-
 export default {
   input: 'tracker/index.js',
   output: {
-    file: `public/${scriptName}.js`,
+    file: 'public/umami.js',
     format: 'iife',
   },
   plugins: [resolve(), buble({ objectAssign: true }), terser({ compress: { evaluate: false } })],
