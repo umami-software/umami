6ea6e2e27d61ce85d4c762c4247272fb435bbe03
Mike Cao <mike@mikecao.com>
2022-03-02 12:10:47 -0800

    Fix tracker build.

--- rollup.tracker.config.js
+++ rollup.tracker.config.js
@@ -1,6 +1,4 @@
-import 'dotenv/config';
 import buble from '@rollup/plugin-buble';
-import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
@@ -9,5 +7,5 @@ export default {
     file: 'public/umami.js',
     format: 'iife',
   },
-  plugins: [resolve(), buble({ objectAssign: true }), terser({ compress: { evaluate: false } })],
+  plugins: [buble({ objectAssign: true }), terser({ compress: { evaluate: false } })],
 };
