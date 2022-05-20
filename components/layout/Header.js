80f285be3ecc049d9bd9354a3338276327220a26
Nils Rugsveen <nirugsveen@gmail.com>
2022-05-20 11:49:52 +0200

    Change header title

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -27,7 +27,7 @@ export default function Header() {
       <header className={classNames(styles.header, 'row')}>
         <div className={styles.title}>
           <Icon icon={<Logo />} size="large" className={styles.logo} />
-          <Link href={isSharePage ? HOMEPAGE_URL : '/'}>umami</Link>
+          <Link href={isSharePage ? HOMEPAGE_URL : '/'}>Headless Analytics</Link>
         </div>
         <HamburgerButton />
         {user && (
