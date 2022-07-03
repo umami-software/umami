c60ec2fb8bfacf60940b06743904e00828cdad7e
rohandebsarkar <rohandebsarkar@gmail.com>
2022-07-03 10:07:36 +0530

    Replace `/api/collect` with collectEndpoint

--- rollup.tracker.config.js
+++ rollup.tracker.config.js
@@ -1,4 +1,5 @@
 import buble from '@rollup/plugin-buble';
+import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
@@ -7,5 +8,14 @@ export default {
     file: 'public/umami.js',
     format: 'iife',
   },
-  plugins: [buble({ objectAssign: true }), terser({ compress: { evaluate: false } })],
+  plugins: [
+    replace({
+      '/api/collect': process.env.API_COLLECT_ENDPOINT
+        ? process.env.API_COLLECT_ENDPOINT
+        : '/api/collect',
+      delimiters: ['', ''],
+    }),
+    buble({ objectAssign: true }),
+    terser({ compress: { evaluate: false } }),
+  ],
 };
