1662b66fbbdfb69f527f09374a484a79f64d9530
Mike Cao <mike@mikecao.com>
2022-08-02 00:52:08 -0700

    Cleaned up script.

--- pages/api/scripts/telemetry.js
+++ pages/api/scripts/telemetry.js
@@ -2,17 +2,17 @@ import { TELEMETRY_PIXEL } from 'lib/constants';
 
 export default function handler(req, res) {
   const { v } = req.query;
+  const script = `
+    (()=>{const i=document.createElement('img');
+      i.setAttribute('src','${TELEMETRY_PIXEL}?v=${v}');
+      i.setAttribute('style','width:0;height:0;position:absolute;pointer-events:none;');
+      document.body.appendChild(i);})();
+  `;
+
   res.setHeader('content-type', 'text/javascript');
   if (process.env.DISABLE_TELEMETRY) {
     res.send('/* telemetry disabled */');
   } else {
-    res.send(
-      `(() => {
-      const i = document.createElement('img');
-      i.setAttribute('src','${TELEMETRY_PIXEL}?v=${v}');
-      i.setAttribute('style','width:0;height:0;position:absolute;pointer-events:none;');
-      document.body.appendChild(i);
-    })();`,
-    );
+    res.send(script.replace(/\s\s+/g, ''));
   }
 }
