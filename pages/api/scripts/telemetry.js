9969c9b6b1a2dd3b9becea38bf56a25f16ebfa91
Mike Cao <mike@mikecao.com>
2022-08-02 00:32:28 -0700

    Forgot the actual check.

--- pages/api/scripts/telemetry.js
+++ pages/api/scripts/telemetry.js
@@ -3,12 +3,16 @@ import { TELEMETRY_PIXEL } from 'lib/constants';
 export default function handler(req, res) {
   const { v } = req.query;
   res.setHeader('content-type', 'text/javascript');
-  res.send(
-    `(() => {
+  if (process.env.DISABLE_TELEMETRY) {
+    res.send('/* telemetry disabled */');
+  } else {
+    res.send(
+      `(() => {
       const i = document.createElement('img');
       i.setAttribute('src','${TELEMETRY_PIXEL}?v=${v}');
       i.setAttribute('style','width:0;height:0;position:absolute;pointer-events:none;');
       document.body.appendChild(i);
     })();`,
-  );
+    );
+  }
 }
