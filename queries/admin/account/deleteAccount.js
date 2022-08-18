30033717521e251c44ac8c38c08cfc6218124fa4
Brian Cao <brianbcao@gmail.com>
2022-08-17 17:35:52 -0700

    remove cascading delete

--- queries/admin/account/deleteAccount.js
+++ queries/admin/account/deleteAccount.js
@@ -2,10 +2,33 @@ import { prisma, runQuery } from 'lib/db';
 
 export async function deleteAccount(user_id) {
   return runQuery(
-    prisma.account.delete({
-      where: {
-        user_id,
-      },
-    }),
+    prisma.$transaction([
+      prisma.pageview.deleteMany({
+        where: { session: { website: { user_id } } },
+      }),
+      prisma.event_data.deleteMany({
+        where: { event: { session: { website: { user_id } } } },
+      }),
+      prisma.event.deleteMany({
+        where: { session: { website: { user_id } } },
+      }),
+      prisma.event.deleteMany({
+        where: { session: { website: { user_id } } },
+      }),
+      prisma.event.deleteMany({
+        where: { session: { website: { user_id } } },
+      }),
+      prisma.session.deleteMany({
+        where: { website: { user_id } },
+      }),
+      prisma.website.deleteMany({
+        where: { user_id },
+      }),
+      prisma.account.delete({
+        where: {
+          user_id,
+        },
+      }),
+    ]),
   );
 }
--- queries/admin/website/deleteWebsite.js
+++ queries/admin/website/deleteWebsite.js
@@ -2,10 +2,28 @@ import { prisma, runQuery } from 'lib/db';
 
 export async function deleteWebsite(website_id) {
   return runQuery(
-    prisma.website.delete({
-      where: {
-        website_id,
-      },
-    }),
+    prisma.$transaction([
+      prisma.pageview.deleteMany({
+        where: { session: { website: { website_id } } },
+      }),
+      prisma.event_data.deleteMany({
+        where: { event: { session: { website: { website_id } } } },
+      }),
+      prisma.event.deleteMany({
+        where: { session: { website: { website_id } } },
+      }),
+      prisma.event.deleteMany({
+        where: { session: { website: { website_id } } },
+      }),
+      prisma.event.deleteMany({
+        where: { session: { website: { website_id } } },
+      }),
+      prisma.session.deleteMany({
+        where: { website: { website_id } },
+      }),
+      prisma.website.delete({
+        where: { website_id },
+      }),
+    ]),
   );
 }
