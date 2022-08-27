54ade11a98b958147cae78381d242d6b76e0e9a0
Brian Cao <brianbcao@gmail.com>
2022-08-26 21:10:46 -0700

    fix send message. look at redis on session create

--- pages/api/heartbeat.js
+++ pages/api/heartbeat.js
@@ -1,16 +1,5 @@
-import { redis } from 'lib/redis';
 import { ok } from 'lib/response';
 
 export default async (req, res) => {
-  console.log('------------------------------------------------------------------------------');
-  // const redis = new createClient({
-  //   url: process.env.REDIS_URL,
-  // });
-
-  //console.log(redis.get);
-
-  const value = await redis.get('session:77c0b9de-677a-5268-8543-6018f0776a81');
-  console.log('complete');
-
-  return ok(res, value);
+  return ok(res, 'nice');
 };
