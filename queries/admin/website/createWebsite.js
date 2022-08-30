f3312732dc0f6502f8effe58db8c93496a1b0df0
Brian Cao <brianbcao@gmail.com>
2022-08-30 15:54:33 -0700

    fix redis.client call

--- queries/admin/website/createWebsite.js
+++ queries/admin/website/createWebsite.js
@@ -15,7 +15,7 @@ export async function createWebsite(user_id, data) {
     })
     .then(async res => {
       if (redis.client && res) {
-        await redis.set(`website:${res.website_uuid}`, Number(res.website_id));
+        await redis.client.set(`website:${res.website_uuid}`, res.website_id);
       }
 
       return res;
