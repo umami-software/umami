6f6cbaa09805af605520c9a0b499aab2f1c0a57e
刘岗 <seesawliu@gmail.com>
2022-10-08 13:46:50 +0800

    Update getWebsiteByUuid.js
    if redis website set from getWebsiteByUuid,the value is 1 forever

--- queries/admin/website/getWebsiteByUuid.js
+++ queries/admin/website/getWebsiteByUuid.js
@@ -10,7 +10,7 @@ export async function getWebsiteByUuid(website_uuid) {
     })
     .then(async res => {
       if (redis.client && res) {
-        await redis.client.set(`website:${res.website_uuid}`, 1);
+        await redis.client.set(`website:${res.website_uuid}`, res.website_id);
       }
 
       return res;
