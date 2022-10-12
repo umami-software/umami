9147415761ba60875ddeb7220b0f1e09015860e4
Brian Cao <brianbcao@gmail.com>
2022-10-12 12:20:20 -0700

    add index to uuid

--- db/postgresql/migrations/04_account_uuid/migration.sql
+++ db/postgresql/migrations/04_account_uuid/migration.sql
@@ -10,3 +10,12 @@ ALTER TABLE "account" ALTER COLUMN "account_uuid" SET NOT NULL;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "account_account_uuid_key" ON "account"("account_uuid");
+
+-- CreateIndex
+CREATE INDEX "account_account_uuid_idx" ON "account"("account_uuid");
+
+-- CreateIndex
+CREATE INDEX "session_session_uuid_idx" ON "session"("session_uuid");
+
+-- CreateIndex
+CREATE INDEX "website_website_uuid_idx" ON "website"("website_uuid");
