8b64ef1a4e84ce004807c550c53c87eede7559e7
Mike Cao <mike@mikecao.com>
2022-10-25 15:48:49 -0700

    Fixed issue with accessing user dashboards. Closes #1590

--- pages/api/accounts/index.js
+++ pages/api/accounts/index.js
@@ -21,9 +21,9 @@ export default async (req, res) => {
   if (req.method === 'POST') {
     const { username, password, account_uuid } = req.body;
 
-    const accountByUsername = await getAccount({ username });
+    const account = await getAccount({ username });
 
-    if (accountByUsername) {
+    if (account) {
       return badRequest(res, 'Account already exists');
     }
 
