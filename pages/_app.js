ab441505321c174f49df0cda2a74d72791ffb6fb
Christian Rackerseder <git@echooff.de>
2023-04-13 08:44:34 +0200

    Prevent indexing content by search engines

--- pages/_app.js
+++ pages/_app.js
@@ -37,6 +37,7 @@ export default function App({ Component, pageProps }) {
         <meta name="theme-color" content="#fafafa" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#2f2f2f" media="(prefers-color-scheme: dark)" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="noindex,nofollow" />
       </Head>
       <div className="container" dir={dir}>
         <Component {...pageProps} />
