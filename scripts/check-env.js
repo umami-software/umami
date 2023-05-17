9d4808f5ef92d7cda75446780eff8f8594acb186
Mike Cao <mike@mikecao.com>
2023-05-16 21:13:17 -0700

    Update check-env for CI build.

--- scripts/check-env.js
+++ scripts/check-env.js
@@ -18,7 +18,9 @@ function checkMissing(vars) {
   }
 }
 
-checkMissing(['DATABASE_URL']);
+if (!process.env.SKIP_DB_CHECK && !process.env.DATABASE_TYPE) {
+  checkMissing(['DATABASE_URL']);
+}
 
 if (process.env.CLICKHOUSE_URL) {
   checkMissing(['CA_CERT', 'CLIENT_CERT', 'CLIENT_KEY', 'KAFKA_BROKER', 'KAFKA_URL', 'REDIS_URL']);
