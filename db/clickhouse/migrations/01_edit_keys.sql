be433cd0fd6f2a73ea0fd602f8e794f80f6763dc
Brian Cao <brian@umami.is>
2023-07-05 21:09:16 -0700

    update CH migration.

--- db/clickhouse/migrations/01_edit_keys.sql
+++ db/clickhouse/migrations/01_edit_keys.sql
@@ -1,4 +1,9 @@
+-- edit event_data values
 ALTER TABLE "event_data" RENAME COLUMN "event_date_value" TO "date_value";
-ALTER TABLE "event_data" RENAME COLUMN "event_numeric_value" TO "numeric_value";
+ALTER TABLE "event_data" RENAME COLUMN "event_numeric_value" TO "number_value";
 ALTER TABLE "event_data" RENAME COLUMN "event_string_value" TO "string_value";
 ALTER TABLE "event_data" RENAME COLUMN "event_data_type" TO "data_type";
+
+-- add job_id
+ALTER TABLE "website_event" ADD COLUMN "job_id" UUID AFTER "created_at";
+ALTER TABLE "event_data" ADD COLUMN "job_id" UUID AFTER "created_at";
