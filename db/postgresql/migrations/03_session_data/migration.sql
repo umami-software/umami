0a3ee2277a719a5f910009d73f42f7ab55c9f79f
Brian Cao <brian@umami.is>
2023-07-06 21:02:56 -0700

    Fix numeric to number.

--- db/postgresql/migrations/03_session_data/migration.sql
+++ db/postgresql/migrations/03_session_data/migration.sql
@@ -2,7 +2,7 @@
 ALTER TABLE "event_data" RENAME COLUMN "event_data_type" TO "data_type";
 ALTER TABLE "event_data" RENAME COLUMN "event_date_value" TO "date_value";
 ALTER TABLE "event_data" RENAME COLUMN "event_id" TO "event_data_id";
-ALTER TABLE "event_data" RENAME COLUMN "event_numeric_value" TO "numeric_value";
+ALTER TABLE "event_data" RENAME COLUMN "event_numeric_value" TO "number_value";
 ALTER TABLE "event_data" RENAME COLUMN "event_string_value" TO "string_value";
 
 -- CreateTable
@@ -12,7 +12,7 @@ CREATE TABLE "session_data" (
     "session_id" UUID NOT NULL,
     "session_key" VARCHAR(500) NOT NULL,
     "string_value" VARCHAR(500),
-    "numeric_value" DECIMAL(19,4),
+    "number_value" DECIMAL(19,4),
     "date_value" TIMESTAMPTZ(6),
     "data_type" INTEGER NOT NULL,
     "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
