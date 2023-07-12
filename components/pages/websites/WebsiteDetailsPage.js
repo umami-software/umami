5eea66aa53731fc02099bc7db868983fa5fd5bfe
Mike Cao <mike@mikecao.com>
2023-07-11 19:33:34 -0700

    Hide links on share page.

--- components/pages/websites/WebsiteDetailsPage.js
+++ components/pages/websites/WebsiteDetailsPage.js
@@ -1,4 +1,5 @@
 import { Loading } from 'react-basics';
+import { useRouter } from 'next/router';
 import Page from 'components/layout/Page';
 import WebsiteChart from 'components/pages/websites/WebsiteChart';
 import FilterTags from 'components/metrics/FilterTags';
@@ -11,6 +12,8 @@ import { WebsiteMetricsBar } from './WebsiteMetricsBar';
 
 export default function WebsiteDetailsPage({ websiteId }) {
   const { data: website, isLoading, error } = useWebsite(websiteId);
+  const { pathname } = useRouter();
+  const showLinks = !pathname.includes('/share/');
 
   const {
     query: { view, url, referrer, os, browser, device, country, region, city, title },
@@ -18,7 +21,7 @@ export default function WebsiteDetailsPage({ websiteId }) {
 
   return (
     <Page loading={isLoading} error={error}>
-      <WebsiteHeader websiteId={websiteId} />
+      <WebsiteHeader websiteId={websiteId} showLinks={showLinks} />
       <WebsiteMetricsBar websiteId={websiteId} sticky={true} />
       <WebsiteChart websiteId={websiteId} />
       <FilterTags
