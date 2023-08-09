39dd634708a8813b9050d24364a55c3ef282bb67
Didier Krux <didier@krux.co>
2023-08-09 23:25:00 +0100

    revert

--- next.config.js
+++ next.config.js
@@ -97,20 +97,6 @@ const config = {
   async headers() {
     return [
       {
-        // mathching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-      {
         source: '/:path*',
         headers,
       },
