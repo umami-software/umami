e48610e83d8a3fa30366c57f0fe0a07612d7ff24
Mike Cao <mike@mikecao.com>
2023-08-17 14:03:41 -0700

    Fixed filter select for insights.

--- components/pages/reports/PopupForm.js
+++ components/pages/reports/PopupForm.js
@@ -3,7 +3,11 @@ import styles from './PopupForm.module.css';
 
 export function PopupForm({ className, style, children }) {
   return (
-    <div className={classNames(styles.form, className)} style={style}>
+    <div
+      className={classNames(styles.form, className)}
+      style={style}
+      onClick={e => e.stopPropagation()}
+    >
       {children}
     </div>
   );
