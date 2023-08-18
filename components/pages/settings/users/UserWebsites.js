87cf5d52985e1dbeb0a2b07c86ef443bb4bb2c6d
Brian Cao <brian@umami.is>
2023-08-18 11:10:44 -0700

    Fix UserWebsites.

--- components/pages/settings/users/UserWebsites.js
+++ components/pages/settings/users/UserWebsites.js
@@ -2,12 +2,19 @@ import { Loading } from 'react-basics';
 import useApi from 'hooks/useApi';
 import WebsitesTable from 'components/pages/settings/websites/WebsitesTable';
 import useMessages from 'hooks/useMessages';
+import useApiFilter from 'hooks/useApiFilter';
 
 export function UserWebsites({ userId }) {
   const { formatMessage, messages } = useMessages();
+  const { filter, page, pageSize, handleFilterChange, handlePageChange, handlePageSizeChange } =
+    useApiFilter();
   const { get, useQuery } = useApi();
-  const { data, isLoading } = useQuery(['user:websites', userId], () =>
-    get(`/users/${userId}/websites`),
+  const { data, isLoading } = useQuery(['user:websites', userId, filter, page, pageSize], () =>
+    get(`/users/${userId}/websites`, {
+      filter,
+      page,
+      pageSize,
+    }),
   );
   const hasData = data && data.length !== 0;
 
@@ -17,7 +24,15 @@ export function UserWebsites({ userId }) {
 
   return (
     <div>
-      {hasData && <WebsitesTable data={data} />}
+      {hasData && (
+        <WebsitesTable
+          data={data}
+          onFilterChange={handleFilterChange}
+          onPageChange={handlePageChange}
+          onPageSizeChange={handlePageSizeChange}
+          filterValue={filter}
+        />
+      )}
       {!hasData && formatMessage(messages.noDataAvailable)}
     </div>
   );
