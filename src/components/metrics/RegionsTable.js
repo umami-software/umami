800731f3aa777d8d94c6bbda714945580331d44f
Mike Cao <mike@mikecao.com>
2023-08-21 12:53:07 -0700

    Fixed import.

--- src/components/metrics/RegionsTable.js
+++ src/components/metrics/RegionsTable.js
@@ -5,7 +5,7 @@ import useLocale from 'components/hooks/useLocale';
 import useMessages from 'components/hooks/useMessages';
 import useCountryNames from 'components/hooks/useCountryNames';
 import MetricsTable from './MetricsTable';
-import regions from 'public/iso-3166-2.json';
+import regions from '../../../public/iso-3166-2.json';
 
 export function RegionsTable({ websiteId, ...props }) {
   const { locale } = useLocale();
