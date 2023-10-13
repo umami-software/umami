8cc7657b1bf0689a81374dc8884b81dae5924884
Francis Cao <franciscao@gmail.com>
2023-10-13 12:53:01 -0700

    allow undefined timezone in yup

--- src/lib/yup.ts
+++ src/lib/yup.ts
@@ -2,11 +2,14 @@ import moment from 'moment';
 import * as yup from 'yup';
 import { UNIT_TYPES } from './constants';
 
-export const TimezoneTest = yup.string().test(
-  'timezone',
-  () => `Invalid timezone`,
-  value => moment.tz.zone(value) !== null,
-);
+export const TimezoneTest = yup
+  .string()
+  .default('UTC')
+  .test(
+    'timezone',
+    () => `Invalid timezone`,
+    value => moment.tz.zone(value) !== null,
+  );
 
 export const UnitTypeTest = yup.string().test(
   'unit',
