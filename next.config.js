4aaa056180f968e8c5008f0fa4db2dae1bb45617
Stephane P. Pericat <shortwavaudio@gmail.com>
2024-01-10 21:56:56 -0600

    enable cors

--- next.config.js
+++ next.config.js
@@ -126,6 +126,16 @@ const config = {
         source: '/:path*',
         headers,
       },
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
     ];
   },
   async rewrites() {
