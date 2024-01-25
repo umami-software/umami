525a048b40349194ded1c042d96c8cfad031a439
Ankit Singh Tomar <ankit.singh@donorthreesixty.com>
2024-01-25 12:28:07 +0530

    add audince verifier

--- src/lib/jwtVerifier.ts
+++ src/lib/jwtVerifier.ts
@@ -2,9 +2,9 @@ import { JwtRsaVerifier } from "aws-jwt-verify";
 
 
 export const verifier = JwtRsaVerifier.create({
-  issuer: process.env.COGNITO_ISSUER, // set this to the expected "iss" claim on your JWTs
-  audience:null, // set this to the expected "aud" claim on your JWTs
-  jwksUri:  process.env.COGNITO_JWKS_URI, // set this to the JWKS uri from your OpenID configuration
+  issuer: process.env.COGNITO_ISSUER,
+  audience: process.env.COGNITO_CLIENT_ID, 
+  jwksUri:  process.env.COGNITO_JWKS_URI
 });
 
 
