b6f4f8e6904dfc8cc2a7e7fb34c3d877286f019c
魔王少年 <q267009886.tw@gmail.com>
2024-01-26 00:35:31 +0800

    Fixing issue when there are too many websites, team can't add any.

--- src/app/(main)/settings/teams/[id]/TeamWebsiteAddForm.tsx
+++ src/app/(main)/settings/teams/[id]/TeamWebsiteAddForm.tsx
@@ -51,7 +51,7 @@ export function TeamWebsiteAddForm({
       {isLoading && !hasData && <Loading icon="dots" position="center" />}
       {!isLoading && !hasData && <Empty />}
       {hasData && (
-        <Form onSubmit={handleSubmit} error={error}>
+        <Form onSubmit={handleSubmit} error={error} style={{ maxHeight: '85vh', overflow: 'auto' }}>
           <WebsitesDataTable userId={user.id} showActions={false}>
             <GridColumn name="select" label={formatMessage(labels.selectWebsite)} alignment="end">
               {row => (
