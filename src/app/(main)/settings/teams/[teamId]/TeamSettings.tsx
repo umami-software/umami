cac15b55ae39dc4f09cf2065b3a6fc2125a2f9c6
Mike Cao <moocao@gmail.com>
2024-02-04 20:17:05 -0800

    Fixed component build.

--- src/app/(main)/settings/teams/[teamId]/TeamSettings.tsx
+++ src/app/(main)/settings/teams/[teamId]/TeamSettings.tsx
@@ -1,25 +1,22 @@
 'use client';
-import { useState } from 'react';
-import { Item, Loading, Tabs, Flexbox, Text, Icon } from 'react-basics';
+import { useContext, useState } from 'react';
+import { Item, Tabs, Flexbox, Text, Icon } from 'react-basics';
 import PageHeader from 'components/layout/PageHeader';
 import { ROLES } from 'lib/constants';
 import Icons from 'components/icons';
-import { useLogin, useTeam, useMessages } from 'components/hooks';
+import { useLogin, useMessages } from 'components/hooks';
 import TeamEditForm from './TeamEditForm';
 import TeamMembers from './TeamMembers';
 import TeamWebsites from './TeamWebsites';
 import TeamData from './TeamData';
 import LinkButton from 'components/common/LinkButton';
+import { TeamContext } from 'app/(main)/teams/[teamId]/TeamProvider';
 
 export function TeamSettings({ teamId }: { teamId: string }) {
+  const team = useContext(TeamContext);
   const { formatMessage, labels } = useMessages();
   const { user } = useLogin();
-  const { data: team, isLoading } = useTeam(teamId);
   const [tab, setTab] = useState('details');
-
-  if (isLoading) {
-    return <Loading position="page" />;
-  }
 
   const canEdit = team?.teamUser?.find(
     ({ userId, role }) => role === ROLES.teamOwner && userId === user.id,
@@ -41,7 +38,7 @@ export function TeamSettings({ teamId }: { teamId: string }) {
         <Item key="websites">{formatMessage(labels.websites)}</Item>
         <Item key="data">{formatMessage(labels.data)}</Item>
       </Tabs>
-      {tab === 'details' && <TeamEditForm teamId={teamId} data={team} allowEdit={canEdit} />}
+      {tab === 'details' && <TeamEditForm teamId={teamId} allowEdit={canEdit} />}
       {tab === 'members' && <TeamMembers teamId={teamId} allowEdit={canEdit} />}
       {tab === 'websites' && <TeamWebsites teamId={teamId} allowEdit={canEdit} />}
       {canEdit && tab === 'data' && <TeamData teamId={teamId} />}
