54abab0bdab50ec25fdc83ac94d7248134d8421b
Mike Cao <moocao@gmail.com>
2024-02-17 10:37:29 -0800

    Added suspense boundary.

--- src/app/sso/SSOPage.tsx
+++ src/app/sso/SSOPage.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { Loading } from 'react-basics';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { setClientAuthToken } from 'lib/client';
+import { Suspense } from 'react';
 
 export default function SSOPage() {
   const router = useRouter();
@@ -18,5 +19,9 @@ export default function SSOPage() {
     }
   }, [router, url, token]);
 
-  return <Loading />;
+  return (
+    <Suspense>
+      <Loading />
+    </Suspense>
+  );
 }
