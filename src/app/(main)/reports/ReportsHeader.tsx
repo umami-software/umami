8fbd6871f767842a9ec616c141b6b0baae9a672c
Francis Cao <franciscao@gmail.com>
2024-02-22 15:31:32 -0800

    Fix report create for view only, fix createdby on save and add column in team websites table

--- src/app/(main)/reports/ReportsHeader.tsx
+++ src/app/(main)/reports/ReportsHeader.tsx
@@ -1,20 +1,25 @@
 import PageHeader from 'components/layout/PageHeader';
 import { Icon, Icons, Text } from 'react-basics';
-import { useMessages, useTeamUrl } from 'components/hooks';
+import { useLogin, useMessages, useTeamUrl } from 'components/hooks';
 import LinkButton from 'components/common/LinkButton';
+import { ROLES } from 'lib/constants';
 
 export function ReportsHeader() {
   const { formatMessage, labels } = useMessages();
   const { renderTeamUrl } = useTeamUrl();
+  const { user } = useLogin();
+  const canEdit = user.role !== ROLES.viewOnly;
 
   return (
     <PageHeader title={formatMessage(labels.reports)}>
-      <LinkButton href={renderTeamUrl('/reports/create')} variant="primary">
-        <Icon>
-          <Icons.Plus />
-        </Icon>
-        <Text>{formatMessage(labels.createReport)}</Text>
-      </LinkButton>
+      {canEdit && (
+        <LinkButton href={renderTeamUrl('/reports/create')} variant="primary">
+          <Icon>
+            <Icons.Plus />
+          </Icon>
+          <Text>{formatMessage(labels.createReport)}</Text>
+        </LinkButton>
+      )}
     </PageHeader>
   );
 }
