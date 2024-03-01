2a8437cac965f96a26e839f5c611344c1814e836
Mike Cao <moocao@gmail.com>
2024-03-01 13:25:32 -0800

    Added jest testing.

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+export default {
+  roots: ['./src'],
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
