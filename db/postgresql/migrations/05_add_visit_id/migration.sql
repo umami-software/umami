b36d61609d2df7be2218832aac73365503291e9c
Francis Cao <franciscao@gmail.com>
2024-03-21 09:36:27 -0700

    fix migration bug

--- db/postgresql/migrations/05_add_visit_id/migration.sql
+++ db/postgresql/migrations/05_add_visit_id/migration.sql
@@ -1,11 +1,11 @@
 -- AlterTable
-ALTER TABLE "website_event" ADD COLUMN "session_id" UUID NULL;
+ALTER TABLE "website_event" ADD COLUMN "visit_id" UUID NULL;
 
 UPDATE "website_event"
-SET session_id = uuid_in(overlay(overlay(md5(CONCAT(session_id::text, to_char(date_trunc('hour', created_at), 'YYYY-MM-DD HH24:00:00'))) placing '4' from 13) placing '8' from 17)::cstring)
-WHERE session_id IS NULL;
+SET visit_id = uuid_in(overlay(overlay(md5(CONCAT(session_id::text, to_char(date_trunc('hour', created_at), 'YYYY-MM-DD HH24:00:00'))) placing '4' from 13) placing '8' from 17)::cstring)
+WHERE visit_id IS NULL;
 
-ALTER TABLE "website_event" ALTER COLUMN "session_id" SET NOT NULL;
+ALTER TABLE "website_event" ALTER COLUMN "visit_id" SET NOT NULL;
 
 -- CreateIndex
 CREATE INDEX "website_event_visit_id_idx" ON "website_event"("visit_id");
