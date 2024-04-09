f6772243517b276982d9df6d8e1a4c12ef02ecc4
badrAG <badrag6@gmail.com>
2024-04-09 18:29:40 +0000

    DEV :: same styles

--- src/app/share/[...shareId]/page.tsx
+++ src/app/share/[...shareId]/page.tsx
@@ -1,5 +1,26 @@
+import { useEffect } from 'react';
 import SharePage from './SharePage';
 
 export default function ({ params: { shareId } }) {
+  useEffect(() => {
+    function sendHeightToParent() {
+      const height = document.body.scrollHeight;
+      window.parent.postMessage({ height: height }, '*');
+    }
+
+    sendHeightToParent();
+    
+    // Add event listener for resize
+    window.addEventListener('resize', () => {
+      sendHeightToParent();
+    });
+
+    // Cleanup function to remove event listener
+    return () => {
+      window.removeEventListener('resize', () => {
+        sendHeightToParent();
+      });
+    };
+  }, [])
   return <SharePage shareId={shareId[0]} />;
 }
