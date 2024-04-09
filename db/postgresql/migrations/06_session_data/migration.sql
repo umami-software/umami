31b059d7db00f4f67ad31d75d977ccd07754109d
Francis Cao <franciscao@gmail.com>
2024-04-08 20:29:16 -0700

    fix 06_session_data migration bug

--- db/postgresql/migrations/06_session_data/migration.sql
+++ db/postgresql/migrations/06_session_data/migration.sql
@@ -5,7 +5,7 @@ DROP INDEX IF EXISTS "event_data_website_id_created_at_event_key_idx";
 ALTER TABLE "event_data" RENAME COLUMN "event_key" TO "data_key";
 
 -- AlterTable
-ALTER TABLE "session_data" DROP COLUMN "deleted_at",
+ALTER TABLE "session_data" DROP COLUMN "deleted_at";
 ALTER TABLE "session_data" RENAME COLUMN "session_key" TO "data_key";
 
 -- CreateIndex
