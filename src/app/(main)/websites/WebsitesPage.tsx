4b67d10f045337bebb0c9569c8f101de38e86220
Mike Cao <moocao@gmail.com>
2024-06-01 14:06:17 -0700

    Fixed teams urls.

--- src/app/(main)/websites/WebsitesPage.tsx
+++ src/app/(main)/websites/WebsitesPage.tsx
@@ -1,8 +1,11 @@
 'use client';
 import WebsitesHeader from 'app/(main)/settings/websites/WebsitesHeader';
 import WebsitesDataTable from 'app/(main)/settings/websites/WebsitesDataTable';
+import { useTeamUrl } from 'components/hooks';
 
-export default function WebsitesPage({ teamId }: { teamId: string }) {
+export default function WebsitesPage() {
+  const { teamId } = useTeamUrl();
+
   return (
     <>
       <WebsitesHeader teamId={teamId} allowCreate={false} />
