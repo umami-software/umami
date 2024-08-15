439377f5c526f84916740bc6b2b25a1151c9fe38
Francis Cao <franciscao@gmail.com>
2024-08-15 12:04:48 -0700

    add final keyword to session data queries

--- src/queries/analytics/sessions/getSessionDataValues.ts
+++ src/queries/analytics/sessions/getSessionDataValues.ts
@@ -51,7 +51,7 @@ async function clickhouseQuery(
               data_type = 4, toString(date_trunc('hour', date_value)),
               string_value) as "value",
       count(*) as "total"
-    from session_data
+    from session_data final
     where website_id = {websiteId:UUID}
       and created_at between {startDate:DateTime64} and {endDate:DateTime64}
       and data_key = {propertyName:String}
