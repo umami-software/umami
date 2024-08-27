b1caf6edb702f227220e0f2e1742963b282be746
Francis Cao <franciscao@gmail.com>
2024-08-27 12:02:23 -0700

    fix monthly and all time charts

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -36,7 +36,7 @@ export function BarChart(props: BarChartProps) {
         x: {
           type: XAxisType,
           stacked: true,
-          min: minDate,
+          min: unit === 'minute' ? minDate : '',
           max: maxDate,
           time: {
             unit,
--- src/queries/analytics/pageviews/getPageviewStats.ts
+++ src/queries/analytics/pageviews/getPageviewStats.ts
@@ -42,7 +42,7 @@ async function clickhouseQuery(
   filters: QueryFilters,
 ): Promise<{ x: string; y: number }[]> {
   const { timezone = 'utc', unit = 'day' } = filters;
-  const { parseFilters, rawQuery } = clickhouse;
+  const { parseFilters, rawQuery, getDateSQL } = clickhouse;
   const { filterQuery, params } = await parseFilters(websiteId, {
     ...filters,
     eventType: EVENT_TYPE.pageView,
@@ -57,7 +57,7 @@ async function clickhouseQuery(
       g.y as y
     from (
       select
-        date_trunc('${unit}', created_at, '${timezone}') as t,
+        ${getDateSQL('website_event.created_at', unit, timezone)} as t,
         count(*) as y
       from website_event
       where website_id = {websiteId:UUID}
@@ -75,7 +75,7 @@ async function clickhouseQuery(
       g.y as y
     from (
       select
-        date_trunc('${unit}', created_at, '${timezone}') as t,
+        ${getDateSQL('website_event.created_at', unit, timezone)} as t,
         sum(views)as y
       from website_event_stats_hourly website_event
       where website_id = {websiteId:UUID}
