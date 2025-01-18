0e4f4affea3620a44ce1ce8e8f240ae86d64d5f6
Mike Cao <moocao@gmail.com>
2025-01-18 02:11:06 -0800

    Added try catch to redis methods.

--- src/lib/load.ts
+++ src/lib/load.ts
@@ -1,3 +1,4 @@
+import { serializeError } from 'serialize-error';
 import { getWebsiteSession, getWebsite } from 'queries';
 import { Website, Session } from '@prisma/client';
 import { getClient, redisEnabled } from '@umami/redis-client';
@@ -5,16 +6,21 @@ import { getClient, redisEnabled } from '@umami/redis-client';
 export async function fetchWebsite(websiteId: string): Promise<Website> {
   let website = null;
 
-  if (redisEnabled) {
-    const redis = getClient();
+  try {
+    if (redisEnabled) {
+      const redis = getClient();
 
-    website = await redis.fetch(`website:${websiteId}`, () => getWebsite(websiteId), 86400);
-  } else {
-    website = await getWebsite(websiteId);
-  }
+      website = await redis.fetch(`website:${websiteId}`, () => getWebsite(websiteId), 86400);
+    } else {
+      website = await getWebsite(websiteId);
+    }
 
-  if (!website || website.deletedAt) {
-    return null;
+    if (!website || website.deletedAt) {
+      return null;
+    }
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log('FETCH WEBSITE ERROR:', serializeError(e));
   }
 
   return website;
@@ -23,16 +29,21 @@ export async function fetchWebsite(websiteId: string): Promise<Website> {
 export async function fetchSession(websiteId: string, sessionId: string): Promise<Session> {
   let session = null;
 
-  if (redisEnabled) {
-    const redis = getClient();
+  try {
+    if (redisEnabled) {
+      const redis = getClient();
 
-    session = await redis.fetch(
-      `session:${sessionId}`,
-      () => getWebsiteSession(websiteId, sessionId),
-      86400,
-    );
-  } else {
-    session = await getWebsiteSession(websiteId, sessionId);
+      session = await redis.fetch(
+        `session:${sessionId}`,
+        () => getWebsiteSession(websiteId, sessionId),
+        86400,
+      );
+    } else {
+      session = await getWebsiteSession(websiteId, sessionId);
+    }
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log('FETCH SESSION ERROR:', serializeError(e));
   }
 
   if (!session) {
