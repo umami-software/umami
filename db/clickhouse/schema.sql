6a8b804aff09410553ad70cc00391129dae649e0
badrAG <badrag6@gmail.com>
2025-02-24 20:00:01 +0000

    upgrade

--- db/clickhouse/schema.sql
+++ db/clickhouse/schema.sql
@@ -26,12 +26,15 @@ CREATE TABLE umami.website_event
     --events
     event_type UInt32,
     event_name String,
+    tag String,
     created_at DateTime('UTC'),
-    job_id UUID
+    job_id Nullable(UUID)
 )
-    engine = MergeTree
-        ORDER BY (website_id, session_id, created_at)
-        SETTINGS index_granularity = 8192;
+ENGINE = MergeTree
+    PARTITION BY toYYYYMM(created_at)
+    ORDER BY (toStartOfHour(created_at), website_id, session_id, visit_id, created_at)
+    PRIMARY KEY (toStartOfHour(created_at), website_id, session_id, visit_id)
+    SETTINGS index_granularity = 8192;
 
 CREATE TABLE umami.event_data
 (
@@ -40,14 +43,156 @@ CREATE TABLE umami.event_data
     event_id UUID,
     url_path String,
     event_name String,
-    event_key String,
+    data_key String,
     string_value Nullable(String),
-    number_value Nullable(Decimal64(4)), --922337203685477.5625
+    number_value Nullable(Decimal64(4)),
     date_value Nullable(DateTime('UTC')),
     data_type UInt32,
     created_at DateTime('UTC'),
-    job_id UUID
+    job_id Nullable(UUID)
 )
-    engine = MergeTree
-        ORDER BY (website_id, event_id, event_key, created_at)
-        SETTINGS index_granularity = 8192;
+ENGINE = MergeTree
+    ORDER BY (website_id, event_id, data_key, created_at)
+    SETTINGS index_granularity = 8192;
+
+CREATE TABLE umami.session_data
+(
+    website_id UUID,
+    session_id UUID,
+    data_key String,
+    string_value Nullable(String),
+    number_value Nullable(Decimal64(4)),
+    date_value Nullable(DateTime('UTC')),
+    data_type UInt32,
+    created_at DateTime('UTC'),
+    job_id Nullable(UUID)
+)
+ENGINE = ReplacingMergeTree
+    ORDER BY (website_id, session_id, data_key)
+    SETTINGS index_granularity = 8192;
+
+-- stats hourly
+CREATE TABLE umami.website_event_stats_hourly
+(
+    website_id UUID,
+    session_id UUID,
+    visit_id UUID,
+    hostname LowCardinality(String),
+    browser LowCardinality(String),
+    os LowCardinality(String),
+    device LowCardinality(String),
+    screen LowCardinality(String),
+    language LowCardinality(String),
+    country LowCardinality(String),
+    subdivision1 LowCardinality(String),
+    city String,
+    entry_url AggregateFunction(argMin, String, DateTime('UTC')),
+    exit_url AggregateFunction(argMax, String, DateTime('UTC')),
+    url_path SimpleAggregateFunction(groupArrayArray, Array(String)),
+    url_query SimpleAggregateFunction(groupArrayArray, Array(String)),
+    referrer_domain SimpleAggregateFunction(groupArrayArray, Array(String)),
+    page_title SimpleAggregateFunction(groupArrayArray, Array(String)),
+    event_type UInt32,
+    event_name SimpleAggregateFunction(groupArrayArray, Array(String)),
+    views SimpleAggregateFunction(sum, UInt64),
+    min_time SimpleAggregateFunction(min, DateTime('UTC')),
+    max_time SimpleAggregateFunction(max, DateTime('UTC')),
+    tag SimpleAggregateFunction(groupArrayArray, Array(String)),
+    created_at Datetime('UTC')
+)
+ENGINE = AggregatingMergeTree
+    PARTITION BY toYYYYMM(created_at)
+    ORDER BY (
+        website_id,
+        event_type,
+        toStartOfHour(created_at),
+        cityHash64(visit_id),
+        visit_id
+    )
+    SAMPLE BY cityHash64(visit_id);
+
+CREATE MATERIALIZED VIEW umami.website_event_stats_hourly_mv
+TO umami.website_event_stats_hourly
+AS
+SELECT
+    website_id,
+    session_id,
+    visit_id,
+    hostname,
+    browser,
+    os,
+    device,
+    screen,
+    language,
+    country,
+    subdivision1,
+    city,
+    entry_url,
+    exit_url,
+    url_paths as url_path,
+    url_query,
+    referrer_domain,
+    page_title,
+    event_type,
+    event_name,
+    views,
+    min_time,
+    max_time,
+    tag,
+    timestamp as created_at
+FROM (SELECT
+    website_id,
+    session_id,
+    visit_id,
+    hostname,
+    browser,
+    os,
+    device,
+    screen,
+    language,
+    country,
+    subdivision1,
+    city,
+    argMinState(url_path, created_at) entry_url,
+    argMaxState(url_path, created_at) exit_url,
+    arrayFilter(x -> x != '', groupArray(url_path)) as url_paths,
+    arrayFilter(x -> x != '', groupArray(url_query)) url_query,
+    arrayFilter(x -> x != '', groupArray(referrer_domain)) referrer_domain,
+    arrayFilter(x -> x != '', groupArray(page_title)) page_title,
+    event_type,
+    if(event_type = 2, groupArray(event_name), []) event_name,
+    sumIf(1, event_type = 1) views,
+    min(created_at) min_time,
+    max(created_at) max_time,
+    arrayFilter(x -> x != '', groupArray(tag)) tag,
+    toStartOfHour(created_at) timestamp
+FROM umami.website_event
+GROUP BY website_id,
+    session_id,
+    visit_id,
+    hostname,
+    browser,
+    os,
+    device,
+    screen,
+    language,
+    country,
+    subdivision1,
+    city,
+    event_type,
+    timestamp);
+
+-- projections
+ALTER TABLE umami.website_event 
+ADD PROJECTION website_event_url_path_projection (
+SELECT * ORDER BY toStartOfDay(created_at), website_id, url_path, created_at
+);
+
+ALTER TABLE umami.website_event MATERIALIZE PROJECTION website_event_url_path_projection;
+
+ALTER TABLE umami.website_event 
+ADD PROJECTION website_event_referrer_domain_projection (
+SELECT * ORDER BY toStartOfDay(created_at), website_id, referrer_domain, created_at
+);
+
+ALTER TABLE umami.website_event MATERIALIZE PROJECTION website_event_referrer_domain_projection;
--- next.config.js
+++ next.config.js
@@ -1,29 +1,31 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 require('dotenv').config();
-const path = require('path');
 const pkg = require('./package.json');
 
-const basePath = process.env.BASE_PATH || '';
-const forceSSL = process.env.FORCE_SSL || '';
-const collectApiEndpoint = process.env.COLLECT_API_ENDPOINT || '';
-const defaultLocale = process.env.DEFAULT_LOCALE || '';
-const trackerScriptName = process.env.TRACKER_SCRIPT_NAME || '';
-const cloudMode = process.env.CLOUD_MODE || '';
-const cloudUrl = process.env.CLOUD_URL || '';
-const disableLogin = process.env.DISABLE_LOGIN || '';
-const disableUI = process.env.DISABLE_UI || '';
-const hostURL = process.env.HOST_URL || '';
-const privateMode = process.env.PRIVATE_MODE || '';
+const TRACKER_SCRIPT = '/script.js';
+
+const basePath = process.env.BASE_PATH;
+const collectApiEndpoint = process.env.COLLECT_API_ENDPOINT;
+const cloudMode = process.env.CLOUD_MODE;
+const cloudUrl = process.env.CLOUD_URL;
+const corsMaxAge = process.env.CORS_MAX_AGE;
+const defaultLocale = process.env.DEFAULT_LOCALE;
+const disableLogin = process.env.DISABLE_LOGIN;
+const disableUI = process.env.DISABLE_UI;
+const forceSSL = process.env.FORCE_SSL;
+const privateMode = process.env.PRIVATE_MODE;
+const trackerScriptName = process.env.TRACKER_SCRIPT_NAME;
+const trackerScriptURL = process.env.TRACKER_SCRIPT_URL;
 
 const contentSecurityPolicy = [
   `default-src 'self'`,
-  `img-src *`,
+  `img-src * data:`,
   `script-src 'self' 'unsafe-eval' 'unsafe-inline'`,
   `style-src 'self' 'unsafe-inline'`,
-  `connect-src 'self' api.umami.is cloud.umami.is`
+  `connect-src 'self' api.umami.is cloud.umami.is`,
 ];
 
-const headers = [
+const defaultHeaders = [
   {
     key: 'X-DNS-Prefetch-Control',
     value: 'on',
@@ -38,32 +40,57 @@ const headers = [
 ];
 
 if (forceSSL) {
-  headers.push({
+  defaultHeaders.push({
     key: 'Strict-Transport-Security',
     value: 'max-age=63072000; includeSubDomains; preload',
   });
 }
 
+const trackerHeaders = [
+  {
+    key: 'Access-Control-Allow-Origin',
+    value: '*',
+  },
+  {
+    key: 'Cache-Control',
+    value: 'public, max-age=86400, must-revalidate',
+  },
+];
+
+const headers = [
+  {
+    source: '/api/:path*',
+    headers: [
+      { key: 'Access-Control-Allow-Origin', value: '*' },
+      { key: 'Access-Control-Allow-Headers', value: '*' },
+      { key: 'Access-Control-Allow-Methods', value: 'GET, DELETE, POST, PUT' },
+      { key: 'Access-Control-Max-Age', value: corsMaxAge || '86400' },
+    ],
+  },
+  {
+    source: '/:path*',
+    headers: defaultHeaders,
+  },
+  {
+    source: TRACKER_SCRIPT,
+    headers: trackerHeaders,
+  },
+];
+
 const rewrites = [];
+
+if (trackerScriptURL) {
+  rewrites.push({
+    source: TRACKER_SCRIPT,
+    destination: trackerScriptURL,
+  });
+}
 
 if (collectApiEndpoint) {
   rewrites.push({
     source: collectApiEndpoint,
     destination: '/api/send',
   });
-}
-
-if (trackerScriptName) {
-  const names = trackerScriptName?.split(',').map(name => name.trim());
-
-  if (names) {
-    names.forEach(name => {
-      rewrites.push({
-        source: `/${name.replace(/^\/+/, '')}`,
-        destination: '/script.js',
-      });
-    });
-  }
 }
 
 const redirects = [
@@ -83,6 +110,27 @@ const redirects = [
     permanent: true,
   },
 ];
+
+// Adding rewrites + headers for all alternative tracker script names.
+if (trackerScriptName) {
+  const names = trackerScriptName?.split(',').map(name => name.trim());
+
+  if (names) {
+    names.forEach(name => {
+      const normalizedSource = `/${name.replace(/^\/+/, '')}`;
+
+      rewrites.push({
+        source: normalizedSource,
+        destination: TRACKER_SCRIPT,
+      });
+
+      headers.push({
+        source: normalizedSource,
+        headers: trackerHeaders,
+      });
+    });
+  }
+}
 
 if (cloudMode && cloudUrl) {
   redirects.push({
@@ -118,7 +166,6 @@ const config = {
     defaultLocale,
     disableLogin,
     disableUI,
-    hostURL,
     privateMode,
   },
   basePath,
@@ -129,36 +176,26 @@ const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
   webpack(config) {
-    const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.('.svg'));
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ['@svgr/webpack'],
-      },
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    config.resolve.alias['public'] = path.resolve('./public');
-
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.(js|ts)x?$/,
+      use: ['@svgr/webpack'],
+    });
     return config;
   },
   async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers,
-      },
-    ];
+    return headers;
   },
   async rewrites() {
     return [
@@ -166,6 +203,10 @@ const config = {
       {
         source: '/telemetry.js',
         destination: '/api/scripts/telemetry',
+      },
+      {
+        source: '/teams/:teamId/:path((?!settings).*)*',
+        destination: '/:path*',
       },
     ];
   },
