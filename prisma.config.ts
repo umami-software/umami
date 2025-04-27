5cbf5a567cf0e6a1aefe14d5cfd0139a42a31977
Mike Cao <moocao@gmail.com>
2025-04-26 21:25:55 -0700

    Upgrade Prisma v6.6.0.

---
+++ prisma.config.ts
@@ -0,0 +1,7 @@
+import path from 'node:path';
+import { defineConfig } from 'prisma/config';
+
+export default defineConfig({
+  earlyAccess: true,
+  schema: path.join('prisma', 'schema.prisma'),
+});
