9d3c926d69fe3d0eea8654f4e1933d7dc062b632
Mike Cao <mike@mikecao.com>
2025-04-28 21:24:57 -0700

    Removed prisma config.

--- prisma.config.ts
+++
@@ -1,7 +0,0 @@
-import path from 'node:path';
-import { defineConfig } from 'prisma/config';
-
-export default defineConfig({
-  earlyAccess: true,
-  schema: path.join('prisma', 'schema.prisma'),
-});
