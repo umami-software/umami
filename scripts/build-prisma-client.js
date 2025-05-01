6594c31a70a0336a4675af90817c3bf9c672d45b
Mike Cao <moocao@gmail.com>
2025-05-01 04:38:43 -0700

    Use esbuild to build prisma client.

---
+++ scripts/build-prisma-client.js
@@ -0,0 +1,14 @@
+import esbuild from 'esbuild';
+
+esbuild
+  .build({
+    entryPoints: ['src/generated/prisma/client.ts'], // Adjust this to your entry file
+    bundle: true, // Bundle all files into one (optional)
+    outfile: 'dist/generated/prisma/client.js', // Output file
+    platform: 'node', // For Node.js compatibility
+    target: 'es2020', // Target version of Node.js
+    format: 'esm', // Use ESM format
+    sourcemap: true, // Optional: generates source maps for debugging
+    external: [], // Optional: Exclude external dependencies from bundling
+  })
+  .catch(() => process.exit(1));
