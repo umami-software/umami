d2a3ed732a07d586ceb559b358214200c0adb1c0
Francis Cao <franciscao@gmail.com>
2025-05-08 10:04:21 -0700

    fix realtime activity ordering

--- src/queries/sql/getRealtimeData.ts
+++ src/queries/sql/getRealtimeData.ts
@@ -64,7 +64,7 @@ export async function getRealtimeData(
     countries,
     urls,
     referrers,
-    events: events.reverse(),
+    events: events,
     series: {
       views: pageviews,
       visitors: sessions,
