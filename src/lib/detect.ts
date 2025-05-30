ebdd9095ab137f1e0497631bc8df099d4fc1c17e
Exlaso <vedantbhavsar67@gmail.com>
2025-05-30 11:14:03 +0530

    fix: decode location fields in user detection

--- src/lib/detect.ts
+++ src/lib/detect.ts
@@ -5,11 +5,11 @@ import ipaddr from 'ipaddr.js';
 import maxmind from 'maxmind';
 import {
   DESKTOP_OS,
-  MOBILE_OS,
   DESKTOP_SCREEN_WIDTH,
-  LAPTOP_SCREEN_WIDTH,
-  MOBILE_SCREEN_WIDTH,
   IP_ADDRESS_HEADERS,
+  LAPTOP_SCREEN_WIDTH,
+  MOBILE_OS,
+  MOBILE_SCREEN_WIDTH,
 } from './constants';
 
 const MAXMIND = 'maxmind';
@@ -146,9 +146,9 @@ export async function getClientInfo(request: Request, payload: Record<string, an
   const userAgent = payload?.userAgent || request.headers.get('user-agent');
   const ip = payload?.ip || getIpAddress(request.headers);
   const location = await getLocation(ip, request.headers, !!payload?.ip);
-  const country = location?.country;
-  const region = location?.region;
-  const city = location?.city;
+  const country = decodeURIComponent(location?.country);
+  const region = decodeURIComponent(location?.region);
+  const city = decodeURIComponent(location?.city);
   const browser = browserName(userAgent);
   const os = detectOS(userAgent) as string;
   const device = getDevice(payload?.screen, os);
