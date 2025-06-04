57acaf9855063f6143d79543fa96acd43231f366
Francis Cao <franciscao@gmail.com>
2025-06-04 16:06:11 -0700

    remove data conversion

--- db/postgresql/migrations/12_update_report_parameter/migration.sql
+++ db/postgresql/migrations/12_update_report_parameter/migration.sql
@@ -1,7 +1,3 @@
--- ConvertData
-UPDATE "report"
-SET "parameters" = CONCAT('"', REPLACE(parameters, '"', '\"'), '"');
-
 -- AlterTable
 ALTER TABLE "report"
 ALTER COLUMN "parameters" SET DATA TYPE JSONB USING parameters::JSONB;
