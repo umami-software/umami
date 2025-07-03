152ad6a1a59c167dc248d39c5fca3a177f765700
Yazeed <138618684+YIOrainy@users.noreply.github.com>
2025-07-03 20:49:01 +0300

    Merge pull request #1 from rekaz-io/feat
    feat: add headers for CF-Access-Client-Id and CF-Access-Client-Secret…

--- src/components/hooks/queries/useEventDataProperties.ts
+++ src/components/hooks/queries/useEventDataProperties.ts
@@ -11,7 +11,7 @@ export function useEventDataProperties(
 
   return useQuery<any>({
     queryKey: ['websites:event-data:properties', { websiteId, ...params }],
-    queryFn: () => get(`/websites/${websiteId}/event-data/properties`, { ...params }),
+    queryFn: () => get(`/websites/${websiteId}/event-data/properties`, { ...params }, { headers: { 'CF-Access-Client-Id': '571942449727ad914a422562e7931a4a.access', 'CF-Access-Client-Secret': '571942449727ad914a422562e7931a4a.secret' } }),
     enabled: !!websiteId,
     ...options,
   });
