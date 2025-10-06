aa376d25dfa19479ea9f0f102a038ae00f57d62d
Francis Cao <franciscao@gmail.com>
2025-10-05 17:39:36 -0700

    Fix margin on website edit

--- src/app/(main)/settings/websites/[websiteId]/WebsiteSettingsPage.tsx
+++ src/app/(main)/settings/websites/[websiteId]/WebsiteSettingsPage.tsx
@@ -2,12 +2,15 @@
 import { WebsiteProvider } from '@/app/(main)/websites/WebsiteProvider';
 import { WebsiteSettings } from '@/app/(main)/websites/[websiteId]/settings/WebsiteSettings';
 import { WebsiteSettingsHeader } from '@/app/(main)/websites/[websiteId]/settings/WebsiteSettingsHeader';
+import { Column } from '@umami/react-zen';
 
 export function WebsiteSettingsPage({ websiteId }: { websiteId: string }) {
   return (
     <WebsiteProvider websiteId={websiteId}>
-      <WebsiteSettingsHeader />
-      <WebsiteSettings websiteId={websiteId} />
+      <Column margin="2">
+        <WebsiteSettingsHeader />
+        <WebsiteSettings websiteId={websiteId} />
+      </Column>
     </WebsiteProvider>
   );
 }
