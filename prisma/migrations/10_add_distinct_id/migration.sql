b19cebcd3e75b1d607cc7431c962ec0d264dabdb
chen86860 <8399882+chen86860@users.noreply.github.com>
2025-11-26 14:50:05 +0800

    Remove all migration files and the migration lock file from the Prisma migrations directory.

--- prisma/migrations/10_add_distinct_id/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "session" ADD COLUMN     "distinct_id" VARCHAR(50);
-
--- AlterTable
-ALTER TABLE "session_data" ADD COLUMN     "distinct_id" VARCHAR(50);
--- prisma/migrations/12_update_report_parameter/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "report"
-ALTER COLUMN "parameters" SET DATA TYPE JSONB USING parameters::JSONB;
