e5a5aeecb5ac30156399c3c342d23df7e45ca069
Prince EKPINSE <princeekpinse+1@gmail.com>
2025-11-29 16:54:45 +0100

    feat(#3748): redirect user to last selected team on login

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -25,7 +25,6 @@ export function LoginForm() {
       onSuccess: async ({ token, user }) => {
         setClientAuthToken(token);
         setUser(user);
-
         router.push('/');
       },
     });
