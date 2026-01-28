752f395d831059632dc2c6bf8e692d4cbffbdf46
Francis Cao <franciscao@gmail.com>
2026-01-27 22:47:29 -0800

    revert websitelayout changes

--- src/app/(main)/websites/[websiteId]/WebsiteLayout.tsx
+++ src/app/(main)/websites/[websiteId]/WebsiteLayout.tsx
@@ -8,25 +8,23 @@ import { WebsiteNav } from './WebsiteNav';
 
 export function WebsiteLayout({ websiteId, children }: { websiteId: string; children: ReactNode }) {
   return (
-    <Column backgroundColor="2">
-      <WebsiteProvider websiteId={websiteId}>
-        <Grid columns={{ xs: '1fr', lg: 'auto 1fr' }} width="100%" height="100%">
-          <Column
-            display={{ xs: 'none', lg: 'flex' }}
-            width="240px"
-            height="100%"
-            border="right"
-            backgroundColor
-            marginRight="2"
-          >
-            <WebsiteNav websiteId={websiteId} />
-          </Column>
-          <PageBody gap>
-            <WebsiteHeader showActions />
-            <Column>{children}</Column>
-          </PageBody>
-        </Grid>
-      </WebsiteProvider>
-    </Column>
+    <WebsiteProvider websiteId={websiteId}>
+      <Grid columns={{ xs: '1fr', lg: 'auto 1fr' }} width="100%" height="100%">
+        <Column
+          display={{ xs: 'none', lg: 'flex' }}
+          width="240px"
+          height="100%"
+          border="right"
+          backgroundColor
+          marginRight="2"
+        >
+          <WebsiteNav websiteId={websiteId} />
+        </Column>
+        <PageBody gap>
+          <WebsiteHeader showActions />
+          <Column>{children}</Column>
+        </PageBody>
+      </Grid>
+    </WebsiteProvider>
   );
 }
