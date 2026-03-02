ddddd5e6bceadf25b84214b4b542ab1fe7e8e9fe
Francis Cao <franciscao@gmail.com>
2026-03-02 13:21:14 -0800

    add event-data GET route

--- src/queries/sql/index.ts
+++ src/queries/sql/index.ts
@@ -1,3 +1,4 @@
+export * from './events/getEventDataById';
 export * from './events/getEventDataEvents';
 export * from './events/getEventDataFields';
 export * from './events/getEventDataProperties';
