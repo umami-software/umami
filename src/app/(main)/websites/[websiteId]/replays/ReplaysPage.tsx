3659c66161a5ddcc30a0cd1687c17f52b2108660
Mike Cao <mike@umami.is>
2026-03-19 17:38:10 -0700

    Allow replays access for users without billing subscription

--- src/app/(main)/websites/[websiteId]/replays/ReplaysPage.tsx
+++ src/app/(main)/websites/[websiteId]/replays/ReplaysPage.tsx
@@ -16,14 +16,14 @@ const KEY_NAME = 'umami.replays.tab';
 export function ReplaysPage({ websiteId }: { websiteId: string }) {
   const [tab, setTab] = useState(getItem(KEY_NAME) || 'replays');
   const { t, labels, messages } = useMessages();
-  const { hasFeature, cloudMode } = useSubscription();
+  const { hasFeature, hasSubscription, cloudMode } = useSubscription();
 
   const handleSelect = (value: Key) => {
     setItem(KEY_NAME, value);
     setTab(value);
   };
 
-  if (cloudMode && !hasFeature('replays')) {
+  if (cloudMode && hasSubscription && !hasFeature('replays')) {
     return (
       <Column gap="3">
         <Panel>
