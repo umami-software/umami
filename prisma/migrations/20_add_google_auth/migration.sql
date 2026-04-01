2a151f1ea440908c707a3db4b326010ddd2cf133
Rudy B. <btte.rudy@gmail.com>
2026-04-01 18:23:37 +0200

    fix: regenerate prisma migration

--- prisma/migrations/20_add_google_auth/migration.sql
+++ prisma/migrations/20_add_google_auth/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE IF NOT EXISTS "website_google_auth" (
+CREATE TABLE "website_google_auth" (
     "google_auth_id" UUID NOT NULL,
     "website_id" UUID NOT NULL,
     "access_token" TEXT NOT NULL,
@@ -7,12 +7,11 @@ CREATE TABLE IF NOT EXISTS "website_google_auth" (
     "expires_at" TIMESTAMPTZ(6) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "property_url" VARCHAR(500),
-    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMPTZ(6),
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
 
-    CONSTRAINT "website_google_auth_pkey" PRIMARY KEY ("google_auth_id"),
-    CONSTRAINT "website_google_auth_website_id_key" UNIQUE ("website_id")
+    CONSTRAINT "website_google_auth_pkey" PRIMARY KEY ("google_auth_id")
 );
 
 -- CreateIndex
-CREATE INDEX IF NOT EXISTS "website_google_auth_website_id_idx" ON "website_google_auth"("website_id");
+CREATE UNIQUE INDEX "website_google_auth_website_id_key" ON "website_google_auth"("website_id");
