8b04be48dea69ae0deb650c2a87145c969c580be
Mike Cao <mike@umami.is>
2026-04-15 22:02:48 -0700

    Revert next config.

--- next.config.ts
+++ next.config.ts
@@ -197,7 +197,7 @@ export default withNextIntl({
     selfRecord,
   },
   basePath,
-  output: process.env.VERCEL ? undefined : 'standalone',
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
