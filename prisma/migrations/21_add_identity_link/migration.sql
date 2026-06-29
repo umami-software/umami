bc2c806e451cab676761a608e12e597b492d2ed3
Artjoms Stukans <artjoms.stukans@gmail.com>
2026-06-29 08:01:25 +0200

    feat: add identity_link migration for postgresql

---
+++ prisma/migrations/21_add_identity_link/migration.sql
@@ -0,0 +1,23 @@
+-- AlterTable
+ALTER TABLE "session" ADD COLUMN "visitor_id" VARCHAR(50);
+
+-- CreateIndex
+CREATE INDEX "session_website_id_visitor_id_idx" ON "session"("website_id", "visitor_id");
+
+-- CreateTable
+CREATE TABLE "identity_link" (
+    "identity_link_id" UUID NOT NULL,
+    "website_id" UUID NOT NULL,
+    "visitor_id" VARCHAR(50) NOT NULL,
+    "distinct_id" VARCHAR(50) NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "linked_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "identity_link_pkey" PRIMARY KEY ("identity_link_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "identity_link_identity_link_id_key" ON "identity_link"("identity_link_id");
+CREATE UNIQUE INDEX "identity_link_website_id_visitor_id_distinct_id_key" ON "identity_link"("website_id", "visitor_id", "distinct_id");
+CREATE INDEX "identity_link_website_id_distinct_id_idx" ON "identity_link"("website_id", "distinct_id");
+CREATE INDEX "identity_link_website_id_visitor_id_idx" ON "identity_link"("website_id", "visitor_id");
