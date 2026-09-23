769e3aca39fc21f4870ea74e91d2881a5553ba0f
Emma Zimbinski <emma@zimbin.ski>
2026-09-22 22:33:40 -0500

    Make EventChart hidden legend items more color blind friendly, introduce shift click to hide all

--- src/components/metrics/Legend.tsx
+++ src/components/metrics/Legend.tsx
@@ -1,13 +1,14 @@
 import { Row, StatusLight, Text } from '@umami/react-zen';
 import type { LegendItem } from 'chart.js/auto';
 import { colord } from 'colord';
+import styles from './Legend.module.css';
 
 export function Legend({
   items = [],
   onClick,
 }: {
   items: any[];
-  onClick: (index: LegendItem) => void;
+  onClick: (index: LegendItem, shiftkey: boolean) => void;
 }) {
   if (!items.find(({ text }) => text)) {
     return null;
@@ -17,11 +18,12 @@ export function Legend({
     <Row gap wrap="wrap" justifyContent="center">
       {items.map(item => {
         const { text, fillStyle, hidden } = item;
-        const color = colord(fillStyle);
+        const color = hidden ? colord(fillStyle).desaturate(1) : colord(fillStyle);
 
         return (
-          <Row key={text} onClick={() => onClick(item)}>
-            <StatusLight color={color.alpha(color.alpha() + 0.2).toHex()}>
+          <Row key={text} onClick={(e: React.MouseEvent) => onClick(item, e.shiftKey)}>
+            <StatusLight className={styles.statusLight} color={color.alpha(color.alpha() + 0.2).toHex()} >
+              <span className={styles.hiddenMark}>{hidden ? 'X' : ''}</span>
               <Text
                 size="sm"
                 color={hidden ? 'disabled' : undefined}
@@ -34,6 +36,6 @@ export function Legend({
           </Row>
         );
       })}
-    </Row>
+    </Row >
   );
 }
